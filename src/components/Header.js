178e4120b848c197c8d0f5ff8636848207baccc9
Angel Martinez <angel.codes139@gmail.com>
2020-11-08 22:59:51 -0600

    new product form

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     <nav className="py-8 w-full bg-primary">
       <div className="items-ce container flex items-center justify-between mx-auto text-white">
         <h1 className="text-4xl font-bold">
-          <Link to={'/'}>Product Manager - Redux</Link>
+          <Link to="/">Product Manager - Redux</Link>
         </h1>
         <Link
           className="px-6 py-3 font-bold bg-secondary rounded-lg uppercase"
