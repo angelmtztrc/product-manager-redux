b1bd570e5025b9e0344f029577c52128e01eb189
Angel Martinez <angel.codes139@gmail.com>
2020-11-09 18:41:34 -0600

    table for products added and form for edit products added

--- src/components/EditProduct.js
+++ src/components/EditProduct.js
@@ -1,5 +1,38 @@
 const EditProduct = () => {
-  return <h1>From Edit product</h1>;
+  return (
+    <div className="mx-auto p-8 w-full max-w-lg border border-gray-100 rounded-lg shadow">
+      <h2 className="text-center text-3xl font-semibold">Edit Product</h2>
+      <form className="mt-5 w-full">
+        <div className="mb-4">
+          <label className="block mb-2 text-sm font-bold">Product name:</label>
+          <input
+            name="name"
+            type="text"
+            placeholder="Product name"
+            className="block px-4 py-2 w-full rounded-lg focus:outline-none shadow focus:shadow-outline appearance-none"
+          />
+        </div>
+        <div className="mb-8">
+          <label className="block mb-2 text-sm font-bold">Product price:</label>
+          <div className="relative">
+            <span className="absolute left-0 top-0 ml-4 mt-2">$</span>
+            <input
+              name="price"
+              type="number"
+              placeholder="Product price"
+              className="block pl-8 pr-4 py-2 w-full rounded-lg focus:outline-none shadow focus:shadow-outline appearance-none"
+            />
+          </div>
+        </div>
+        <button
+          type="submit"
+          className="px-6 py-2 w-full text-white font-bold bg-primary rounded-lg uppercase"
+        >
+          Save
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default EditProduct;
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,5 +1,35 @@
 const Products = () => {
-  return <h1>From Products</h1>;
+  return (
+    <>
+      <h2 className="text-center text-3xl font-semibold">Product Listing</h2>
+      <div className="mt-8 border-b border-gray-200 rounded shadow overflow-hidden">
+        <table className="min-w-full">
+          <thead className="text-white bg-primary">
+            <tr>
+              <th
+                className="px-4 py-3 w-1/3 text-left font-semibold uppercase"
+                scope="col"
+              >
+                Name
+              </th>
+              <th
+                className="px-4 py-3 w-1/3 text-left font-semibold uppercase"
+                scope="col"
+              >
+                Price
+              </th>
+              <th
+                className="px-4 py-3 w-1/3 text-left font-semibold uppercase"
+                scope="col"
+              >
+                Actions
+              </th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+    </>
+  );
 };
 
 export default Products;
