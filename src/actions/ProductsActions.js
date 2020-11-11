1b0a5f0167593b6ebc448386ff3bdf2841d502fb
Angel Martinez <angel.codes139@gmail.com>
2020-11-10 23:00:02 -0600

    modifying the state

--- src/actions/ProductsActions.js
+++ src/actions/ProductsActions.js
@@ -1,7 +1,37 @@
 // constants
+import {
+  ADD_PRODUCT_FAIL,
+  ADD_PRODUCT_INIT,
+  ADD_PRODUCT_SUCCESS
+} from '../constants';
 
 export function addProductAction(product) {
-  return () => {
-    console.log(product);
+  return dispatch => {
+    // initialize the action
+    dispatch(createProductInit());
+
+    try {
+      // when a product is successfully created
+      dispatch(createProductSuccess(product));
+    } catch (error) {
+      // when something is wrong
+      dispatch(createProductFail);
+    }
   };
 }
+
+// initialize create product
+const createProductInit = () => ({
+  type: ADD_PRODUCT_INIT
+});
+
+// on product successfully created
+const createProductSuccess = product => ({
+  type: ADD_PRODUCT_SUCCESS,
+  payload: product
+});
+
+// on something went wrong when creating a product
+const createProductFail = () => ({
+  type: ADD_PRODUCT_FAIL
+});
--- src/reducers/ProductsReducer.js
+++ src/reducers/ProductsReducer.js
@@ -1,5 +1,9 @@
 // constants
-import {} from '../constants';
+import {
+  ADD_PRODUCT_FAIL,
+  ADD_PRODUCT_INIT,
+  ADD_PRODUCT_SUCCESS
+} from '../constants';
 
 // initial state of the reducer
 const initialState = {
@@ -10,6 +14,22 @@ const initialState = {
 
 export default function ProductsReducer(state = initialState, action) {
   switch (action.type) {
+    case ADD_PRODUCT_INIT:
+      return {
+        ...state,
+        loading: true
+      };
+    case ADD_PRODUCT_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        products: [...state.products, action.payload]
+      };
+    case ADD_PRODUCT_FAIL:
+      return {
+        ...state,
+        loading: false
+      };
     default:
       return state;
   }
